861470a3078810fda094bda315382a421eacfaab
Dmitry Tarasov <dtarasov@hse.ru>
2024-02-16 08:51:07 +0300

    fad score problem fixes

--- examples/audio/official_dummy_inference.py
+++ examples/audio/official_dummy_inference.py
@@ -62,7 +62,7 @@ else:
 if dense_dummy_scheduler:
     variant = "q_posterior_official_repo_aux_only_timesteps_importance_sampling_transitioning_matricies_plus_eye2024-01-07 15:36:11.188800"
 else:
-    variant = "q_posterior_official_repo_aux_only2024-01-07 14:37:35.639452"
+    variant = "q_posterior_official_repo_aux_only_dummy_q_posterior_reconstruct_30_monitoring2024-02-05 10:45:21.810084"
 
 model = Transformer2DModel.from_pretrained("ddpm-audio-mnist-128/", variant=variant, use_safetensors=True, num_embeds_ada_norm=100, output_attentions=True)
 assert model.is_input_continuous == False, 'transformer is discrete'
@@ -121,19 +121,6 @@ pipeline_out: AudioCodesPipelineOutput = pipeline(
 )
 audio_codes = pipeline_out.audio_codes
 audio_values = pipeline_out.audio_values
-
-for timestep_i in range(NUM_TRAIN_TIMESTEPS):
-    total_cross_attention_norm = 0
-    total_self_attention_norm = 0
-    for transformer_block_j in range(len(pipeline_out.self_attentions[0])):
-        self_attention = pipeline_out.self_attentions[timestep_i][transformer_block_j]
-        cross_attention = pipeline_out.cross_attentions[timestep_i][transformer_block_j]
-        #
-        total_cross_attention_norm += self_attention.norm(2)
-        total_self_attention_norm  += cross_attention.norm(2)
-    #
-    print(f"{timestep_i}\t self - cross", total_self_attention_norm - total_cross_attention_norm)
-
 
 test_dir = os.path.join(config.output_dir, config.experiment_name, "samples_dummy_q_posterior_no_gumbel_noise")
 generated_samples_path = test_dir
